b09b18ba4f43def3cc6853a63e5660331115c91a
Sholom <boruchsteinmetz@outlook.com>
2018-08-12 15:11:42 -0400

    on new task button click render new task page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {NewTaskPage} from '../pages/new-task/new-task';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NewTaskPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NewTaskPage
+
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { NewTaskPage } from '../new-task/new-task.module';
+import { NewTaskPage } from '../new-task/new-task';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
 
   }
@@ -23,6 +22,10 @@ export class HomePage {
 
 itemSelected(item: string) {
     this.navCtrl.push(NewTaskPage);
+  } 
+
+taskSelected(item: string){
+  
 }
 }
   
