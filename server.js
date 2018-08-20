646294d5f065f4f74025657eb78148b0803f140d
Sholom <boruchsteinmetz@outlook.com>
2018-08-20 12:15:23 -0400

    changed server.js

--- server.js
+++ server.js
@@ -15,26 +15,47 @@
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
 
-var express  = require('express');
-var app      = express();                               
-var morgan = require('morgan');            
-var bodyParser = require('body-parser');    
-var cors = require('cors');
+// var express  = require('express');
+// var app      = express();                               
+// var morgan = require('morgan');            
+// var bodyParser = require('body-parser');    
+// var cors = require('cors');
  
-app.use(morgan('dev'));                                        
-app.use(bodyParser.urlencoded({'extended':'true'}));            
-app.use(bodyParser.json());                                     
-app.use(cors());
+// app.use(morgan('dev'));                                        
+// app.use(bodyParser.urlencoded({'extended':'true'}));            
+// app.use(bodyParser.json());                                     
+// app.use(cors());
  
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
  
+// app.use(express.static('www'));
+// app.set('port', process.env.PORT || 5000);
+// app.listen(app.get('port'), function () {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+
+var express = require('express'),
+    app = express();
+
 app.use(express.static('www'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// API Routes
+// app.get('/blah', routeHandler);
+
 app.set('port', process.env.PORT || 5000);
+
 app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
